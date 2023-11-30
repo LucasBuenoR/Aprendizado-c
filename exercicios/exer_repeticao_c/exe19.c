2d95670336534ba9f8852e567966dcd0511a8b9c
Lucas <lucaslucasbueno2015@outlook.com>
2023-11-29 21:17:09 -0300

    corrigindo exercicios

--- exercicios/exer_repeticao_c/exe19.c
+++ exercicios/exer_repeticao_c/exe19.c
@@ -6,27 +6,37 @@ int main() {
     
     int x, y;
 
-    while (1) {
+    while (1) 
+    {
         // Lê as coordenadas do ponto
         printf("Digite as coordenadas (X Y): ");
         scanf("%d %d", &x, &y);
 
         // Verifica se pelo menos uma coordenada é NULA
-        if (x == 0 || y == 0) {
+        if (x == 0 || y == 0) 
+        {
             break;
         }
 
         // Determina o quadrante
-        if (x > 0 && y > 0) {
+        if (x > 0 && y > 0) 
+        {
             printf("primeiro\n");
-        } else if (x < 0 && y > 0) {
+        } 
+        else if (x < 0 && y > 0) 
+        {
             printf("segundo\n");
-        } else if (x < 0 && y < 0) {
+        } 
+        else if (x < 0 && y < 0) 
+        {
             printf("terceiro\n");
-        } else if (x > 0 && y < 0) {
+        } 
+        else if (x > 0 && y < 0) 
+        {
             printf("quarto\n");
         }
     }
-
+// & - 
+// && - 
     return 0;
 }
